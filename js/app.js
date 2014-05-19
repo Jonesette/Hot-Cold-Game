944676dfac72c9d86c9442b96028dc3c4dd7d35f
Jonesette <sjones@jjaviation.com>
2014-05-18 21:32:34 -0600

    Refactored
    Cleaned up If Statements and
    -Check users guess and ensure it is a valid number before accepting it
    and increasing guess counter. This entails checking if it is a number
    between 1 and 100 and ensuring it isn't nothing or just spaces.
    -Keep a user from continuing to guess after winning.
    -Clear user previous guess from input for quick guessing.
    -Allow user to press enter to submit a guess.
    -Start a new game without page refresh.﻿

--- js/app.js
+++ js/app.js
@@ -13,89 +13,97 @@ $(document).ready(function(){
 
 /*Computer Selects Number 1-100*/
 var answer = Math.floor((Math.random() * 100) +1);
-console.log (answer);
+//console.log (answer);
 
 /*Create User Guess Variable*/
 var Guess;
-//console.log(Guess)
-
-  $( 'form' ).bind('keypress', function(e){
-      if ( e.keyCode == 13 ) {
-        return false;
-      }
-  });
+var Compare;
 
   var numGuesses = 0;
 
+  var newGame = function() {
+    answer = Math.floor((Math.random() * 100) +1);
+    //console.log (answer);
+    $('form').show();
+    $('#guessCount').show(); 
+    numGuesses = 0;
+    $('#count').text(numGuesses);
+    $("#feedback").text("Make your Guess!");
+    $("#guessList").text("");
+    $("#userGuess").val("");
+  };
+
   /* New Game Button (No Server Call */
- $(".new").click(function() {
-  var answer = Math.floor((Math.random() * 100) +1);
-  console.log (answer);
-  numGuesses = 0;
-  $('#count').text(numGuesses);
-  $("#feedback").text("Make your Guess!");
-  $("#guessList").text("");
-  $("#userGuess").val("");
- });
+     $(".new").click(function() {
+      newGame();
+     });
+
+  $('#userGuess').focus(function() {
+    this.value = '';
+  });
+
+  $('#userGuess').click(function() {
+    this.value = '';
+  });
 
   	/*Feedback  div#feedback */
     $('#guessButton').click(function(event) {
       event.preventDefault();
+      Guess = +$("#userGuess").val();
+      Compare = Math.abs(Guess - answer);
+      //console.log(Guess);
+      //console.log(Compare);
+
+      if ((Guess > 100) || (Guess <= 0) || (isNaN(Guess))) {
+      alert("The number must be between 1 and 100!");
+      $("#userGuess").val("");
+      }  
+
+      else {   
       numGuesses++;
-      Guess = $("#userGuess").val();
       $('#count').text(numGuesses);
-      console.log(Guess);
 
-      if (Guess > 100) {
-        alert("The number must be between 1 and 100!");
-        $("#feedback").text("Out of Range.");
-        $("#guessList").append('<li>'+ Guess + ' Out of Range</li>');
-      }
-
-      if (Guess < 0) {
-        alert("The number must be between 1 and 100!");
-        $("#feedback").text("Out of Range.");
-        $("#guessList").append('<li>'+ Guess + ' Out of Range</li>');
-      }
-
-      /* Just Right */
-      if (Guess == answer) {
-        $("#feedback").text("You Got It!!");
-        $("#guessList").append('<li>'+ Guess + ' You Got It!!</li>');
-      }
-
-      /* A Bit Too High */
-      else if ((Guess <= (answer + 20)) && (Guess > answer) && (Guess >= (answer + 10))) {
-  		  $("#feedback").text("You're Getting Warm but Too High");
-        $("#guessList").append('<li>'+ Guess + ' Too High</li>');
-      }
-
-  		/* Much too High */
-      else if (Guess > (answer +20) && (Guess < 100)) {
-  		  $("#feedback").text("You're Cold - Too High");
-        $("#guessList").append('<li>'+ Guess + ' Too High</li>');
-      }
-      
-      /* Oh so Close */ 
-  			/*Low*/
-        else if ((Guess < (answer + 10)) && (Guess > answer)) {
-          $("#feedback").text("You're Very Close but Too High");
-          $("#guessList").append('<li>'+ Guess + ' Too High</li>');
+        /* Just Right */
+        if (Compare == 0) {
+            var correct = confirm("You Win!! Want to Play Again?");
+            if (correct) {
+              newGame();
+            }
+            else {
+              $("#feedback").text("Thanks for Playing!");
+              $('form').hide();
+              $('#guessCount').hide(); 
+            }
         }
-  			/*High*/
-        else if ((Guess > (answer - 10)) && (Guess < answer)) {
-          $("#feedback").text("You're Very Close but Too Low");
+
+        else if (Guess > answer) {
+          if (Compare <=10) {
+            $("#feedback").text("You're Very Close but Too High");
+          }
+          else if (Compare <=20) {
+            $("#feedback").text("You're Getting Warm but Too High");
+          }
+          else {
+            $("#feedback").text("You're Cold - Too High");
+          }
+
+          $("#guessList").append('<li>'+ Guess + ' Too High</li>');
+
+        }
+
+        else {
+          if (Compare <=10) {
+            $("#feedback").text("You're Very Close but Too Low");
+          }
+          else if (Compare <=20) {
+            $("#feedback").text("You're Getting Warm but Too Low");
+          }
+          else {
+            $("#feedback").text("You're Cold - Too Low");
+          }
+
           $("#guessList").append('<li>'+ Guess + ' Too Low</li>');
         }
-  		/* A bit too Low */
-      else if ((Guess >= (answer - 20)) && (Guess < answer) && (Guess <= (answer - 10))) {
-       $("#feedback").text("You're Getting Warm but Too Low");
-       $("#guessList").append('<li>'+ Guess + ' Too Low</li>');
-      }
-  		/* Much too Low */
-      else if (Guess < (answer - 20) && (Guess > 0)) {
-        $("#feedback").text("You're Cold - Too Low");
-        $("#guessList").append('<li>'+ Guess + ' Too Low</li>');
       }
     });
 
